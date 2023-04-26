b11e2e6ed785cb9ca2fad0bda292719f6e840616
Velinov <124807884+TsvetanVelinov@users.noreply.github.com>
2023-04-27 01:00:12 +0300

    NumberInRange
    Lap task

---
+++ 6NumberInRange.js
@@ -0,0 +1,17 @@
+function NumberInRange (input){
+
+    let num = Number (input[0]);
+
+
+
+if (num >= -100 && num <= 100 && num != 0){
+
+    console.log("Yes");
+} else {
+    console.log("No");
+}
+
+}
+
+
+NumberInRange (["0"])
